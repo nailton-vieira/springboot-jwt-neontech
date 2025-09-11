e61c6bb2fce32ab7896600322e0af462b7e3a6b3
nailton-vieira <nailtonvieira@gmail.com>
2025-09-10 23:21:10 -0300

    base e conexão com banco ok

---
+++ src/main/java/com/nailtonvieira/backend_app/config/EnvConfig.java
@@ -0,0 +1,19 @@
+package com.nailtonvieira.backend_app.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class EnvConfig {
+    
+    @PostConstruct
+    public void loadEnv() {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+    }
+}
